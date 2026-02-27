7131a4928ca29649c2c51c20baaac906e3030bcc
shahalammirza <mshahalam1246@gmail.comm>
2026-02-27 16:48:13 +0600

    where we know about scope variable

---
+++ scope.js
@@ -0,0 +1,20 @@
+// function add(a, b){
+//     const factor = 2.5;
+//     const result = a + b + factor;
+//     console.log(result)
+// }
+// add(2, 3)
+// console.log(result)
+
+function sum (a, b){
+    const result = a + b;
+    console.log(result);
+
+    function sub(c,d){
+        const result = c + d;
+        console.log(result)
+    }
+    sub(2, 9)
+}
+
+sum(2, 6);
