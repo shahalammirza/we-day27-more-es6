53987f6ade479c31113c4abdf698b715886447f0
shahalammirza <mshahalam1246@gmail.comm>
2026-02-27 12:25:14 +0600

    added truthy and undefined file

---
+++ truthy.js
@@ -0,0 +1,10 @@
+let num = 0;
+if(!num){
+    console.log('truthy');
+}else{
+    console.log('falsy')
+}
+
+if(!!num){
+    console.log('truthy for !!')
+}
---
+++ undefined.js
@@ -0,0 +1,17 @@
+let num;
+console.log(num);
+
+const sum = (a, b) => {
+    console.log(a, b);
+}
+sum(5);
+
+
+function math (a , b){
+    const result = a + b;
+    return;
+}
+console.log(math());
+
+const name = null;
+console.log(typeof name)
